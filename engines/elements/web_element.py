aa223b81028e88a41d0adca7fc7a61c0da968c7b
ira <larax@ukr.net>
2019-12-02 23:23:29 +0200

    Merge branch 'master' of D:\Simple_Fraimwork with conflicts.

--- engines/elements/web_element.py
+++ engines/elements/web_element.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.keys import Keys
 
 
-class WebElement(object):
+class MyWebElement:
 
     def __init__(self, element):
         self.element = element
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
-from engines.drivers.selenium_driver import SeleniumDriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+from engines.drivers.selenium_driver import MySeleniumDriver
 
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = SeleniumDriver(webdriver.Chrome(executable_path=r'/Users/iryna.yeltsova/chromedriver'))
-    driver.go_to_page()
+    # driver = MySeleniumDriver(webdriver.Chrome(executable_path=r'C:\Users\Irina\PycharmProjects\lib\chromedriver.exe'))
+    # driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+
     yield driver
     driver.quit()
